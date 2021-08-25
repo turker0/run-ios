b3e63880c77333fe849bb0629b7dfc1326bb4256
oguzhan.turker <oguzhan.turker@sabancidx.com>
2021-08-26 02:30:44 +0300

    Add order feature

--- index.js
+++ index.js
@@ -1,51 +1,75 @@
 #! /usr/bin/env node
 
-const commander = require('commander')
+const commander = require("commander");
+
+const Prefered = [
+  "iPhone 12 Pro Max",
+  "iPhone 12 Pro",
+  "iPhone 12",
+  "iPhone 12 mini",
+];
 
 commander
-  .version(require('./package.json').version)
-  .description('Quick React-Native Run-IOS')
-  .action(() => start())
+  .version(require("./package.json").version)
+  .description("Quick React-Native Run-IOS")
+  .action(() => start());
 
-commander.parse(process.argv)
+commander.parse(process.argv);
 
 function start() {
-  if (require('os').platform() == 'darwin') {
-    const inquirer = require('inquirer')
-  
-    const { spawn, exec } = require('child_process')
-  
-    const getIOSDeviceProcess = exec('xcrun simctl list --json devices available')
-  
-    let message = ''
-  
-    getIOSDeviceProcess.stdout.on('data', jsonString => message += jsonString)
-  
-    getIOSDeviceProcess.on('exit', () => {
-      const json = JSON.parse(message)
-  
-      const choices = []
-  
+  if (require("os").platform() == "darwin") {
+    const inquirer = require("inquirer");
+
+    const { spawn, exec } = require("child_process");
+
+    const getIOSDeviceProcess = exec(
+      "xcrun simctl list --json devices available"
+    );
+
+    let message = "";
+
+    getIOSDeviceProcess.stdout.on(
+      "data",
+      (jsonString) => (message += jsonString)
+    );
+
+    getIOSDeviceProcess.on("exit", () => {
+      const json = JSON.parse(message);
+
+      const choices = [];
+
       for (const key of Object.keys(json.devices)) {
-        if (key.startsWith('com.apple.CoreSimulator.SimRuntime.iOS')) {
+        if (key.startsWith("com.apple.CoreSimulator.SimRuntime.iOS")) {
           for (const device of json.devices[key]) {
-            choices.push(device.name)
+            choices.push(device.name);
           }
         }
       }
-  
-      inquirer.prompt([
-        {
-          type: 'list',
-          name: 'device',
-          message: 'Quick React-Native Run-IOS',
-          choices
-        }
-      ])
-      .then(({device}) => spawn('npx', [`react-native`, `run-ios`, `--simulator="${device}"`], {stdio: 'inherit', shell: true}))
-    })
+
+      choices.sort(function (a, b) {
+        return b.localeCompare(a, undefined, {
+          numeric: true,
+          sensitivity: "base",
+        });
+      });
+
+      inquirer
+        .prompt([
+          {
+            type: "list",
+            name: "device",
+            message: "Quick React-Native Run-IOS",
+            choices,
+          },
+        ])
+        .then(({ device }) =>
+          spawn("npx", [`react-native`, `run-ios`, `--simulator="${device}"`], {
+            stdio: "inherit",
+            shell: true,
+          })
+        );
+    });
   } else {
-    console.log('Sorry, run-ios is not supported with your operating system')
+    console.log("Sorry, run-ios is not supported with your operating system");
   }
 }
-  
